eadcc648111914169850873e6a0472c0dfc09bde
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-07-25 20:24:05 -0300

    hamdling exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ForceJsonResponseMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -17,7 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $exception) {
-            return errorResponse(status: 404, message: $exception->getMessage());
+        // $exceptions->render(function (NotFoundHttpException $exception) {
+        //     return errorResponse(status: 404, message: $exception->getMessage());
+        // });
+        $exceptions->render(function (HttpExceptionInterface $exception) {
+            return errorResponse($exception->getStatusCode(), $exception->getMessage());
         });
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('posts', PostController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('hello-world', function () {
+        return response()->json(['message' => 'Hello World']);
+    });
+});
